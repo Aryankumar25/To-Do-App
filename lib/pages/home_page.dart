3601f518f80b67e1b96e91a21b39345031e01849
Aryankumar25 <ay2475@srmist.edu.in>
2024-10-31 04:31:49 +0530

    features done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/util/dialog_box.dart';
 import 'package:to_do_app/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,6 +10,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _controller = TextEditingController();
+
+
+  List toDolist=[
+    ["MAKE TUTORAIL",false],
+    ["do exercise",true],
+  ];
+
+  void checkBoxChanged(bool? value,int index){
+    setState(() {
+      toDolist[index][1] = !toDolist[index][1];
+    });
+  }
+
+  void saveNewTask(){
+    setState(() {
+      toDolist.add([ _controller.text,false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  void createNewTask(){
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
+  }
+
+
+  void deleteTask(int index){
+    setState(() {
+      toDolist.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +61,20 @@ class _HomePageState extends State<HomePage> {
         title: Center(child: Text("TO DO LIST",style: TextStyle(color: Colors.white,fontSize: 24),)),
         elevation: 100,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+        ),
       body: ListView.builder(
-        
-
+        itemCount: toDolist.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskname: toDolist[index][0],
+             taskcompleted: toDolist[index][1],
+              onChanged: (value)=> checkBoxChanged(value, index),
+              deleteFunction: (context) => deleteTask(index),
+          );
+        },
         ),
     );
   }
--- lib/util/todo_tile.dart
+++ lib/util/todo_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 
 class TodoTile extends StatelessWidget {
@@ -6,35 +7,54 @@ class TodoTile extends StatelessWidget {
   final String taskname;
   final bool taskcompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
 
   TodoTile({super.key,
    required this.taskname,
    required this.taskcompleted,
    required this.onChanged,
+   required this.deleteFunction,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Container(
-        padding: EdgeInsets.all(18),
-        child: Row(
+      padding: const EdgeInsets.only(left:25, right:25, top: 25),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            ///checkbox
-            Checkbox(value: taskcompleted, onChanged: onChanged),
-
-
-
-            Text(taskname,style: TextStyle(color: Colors.white,fontSize: 18),),
+          SlidableAction(
+            onPressed: deleteFunction,
+            icon: Icons.delete,
+            backgroundColor: Colors.red.shade300,
+            borderRadius: BorderRadius.circular(12),
+          )
           ],
         ),
-        decoration: BoxDecoration(
-          color: Colors.blueGrey,
-          borderRadius: BorderRadius.circular(12),
+        child: Container(
+          padding: EdgeInsets.all(18),
+          decoration: BoxDecoration(
+            color: Colors.cyan,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              ///checkbox
+              Checkbox(value: taskcompleted, onChanged: onChanged),
+        
+        
+        
+              Text(
+                taskname,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  decoration: taskcompleted? TextDecoration.lineThrough: TextDecoration.none),),
+            ],
+          ),
         ),
-
       ),
     );
   }
